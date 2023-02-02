e5208389558b1976efd987d3625c156118abe179
audunlk <audunlk@gmail.com>
2023-02-02 14:38:59 +0100

    updates

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { getTweets, getTweetsByUsername, createTweet } = require('./services/database');
+const { getTweets, getTweetsByUsername, createTweet, getUserByUsername, updateLikes, handleCheckLikes, defineLiked  } = require('./services/database');
+const jwt = require('jsonwebtoken');
 
 const PORT = 3333;
+const APP_SECRET = 'my-secret-key-1234'
 
 app.use(cors());
 app.use(express.json());
@@ -24,12 +26,114 @@ app.get('/tweets/:username', async (req, res) => {
 });
 
 //post request
-app.post('/tweets/:username', async (req, res) => {
+app.post('/tweets', async (req, res) => {
   const { text } = req.body;
-  const { username } = req.params;
+  const token = req.headers['x-token'];
+
+  try{
+  const payload = jwt.verify(token, Buffer.from(APP_SECRET, 'base64'));
+  const username = payload.username;
+  
   const newTweet = await createTweet(username, text);
   res.json(newTweet);
-}); 
+  }catch(error){
+    res.status(401).json({ error: 'Invalid token' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await getUserByUsername(username);
+
+    if (!user) {
+      res.status(401).send({ error: 'Unknown user - not found' });
+      return;
+    }
+  
+    if (password !== user.password) {
+      res.status(401).send({ error: 'Wrong password' });
+      return;
+    }
+  
+    const token = jwt.sign({
+      id: user.id,
+      username: user.username,
+      name: user.name
+    }, Buffer.from(APP_SECRET, 'base64'));
+  
+    res.json({ token });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+});
+
+app.get('/session', async (req, res) => {
+  const token = req.headers['x-token'];
+  if(!token) {
+    res.status(401).json({ error: 'Invalid token' });
+    return;
+  }
+  try{
+    const payload = jwt.verify(token, Buffer.from(APP_SECRET, 'base64'));
+    res.json({message: `You're logged inn as ${payload.username}`});
+  }catch(error){
+    res.status(401).json({ error: 'Invalid token' });
+  }
+
+});
+
+
+
+
+
+app.post('/tweets/:tweetId/', async (req, res) => {
+  const { tweetId } = req.params;
+  const { like_count } = req.body;
+  const newLike = await updateLikes(tweetId, like_count);
+  res.json(newLike);
+});
+
+app.get('/tweets/:tweetId/likes/:userId', async (req, res) => {
+  const { tweetId, userId } = req.params;
+  const token = req.headers['x-token'];
+  try{
+    const payload = jwt.verify(token, Buffer.from(APP_SECRET, 'base64'));
+    const user_id = payload.id;
+    if(user_id !== userId){
+      res.status(401).json({ error: 'You are not allowed to like this tweet' });
+      return;
+    }
+    const checkLikes = await handleCheckLikes(tweetId, userId);
+    res.json(checkLikes);
+  }catch(error){
+    res.status(401).json({ error: 'Invalid token' });
+  }
+
+});
+
+app.post('/tweets/:tweetId/likes/:userId', async (req, res) => {
+  const { tweetId, userId } = req.params;
+  const { like_count } = req.body;
+  const token = req.headers['x-token'];
+  try{
+    const payload = jwt.verify(token, Buffer.from(APP_SECRET, 'base64'));
+    const user_id = payload.id;
+    if(user_id !== userId){
+      res.status(401).json({ error: 'You are not allowed to like this tweet' });
+      return;
+    }
+    const newLike = await updateLikes(tweetId, like_count);
+    res.json(newLike);
+  }catch(error){
+    res.status(401).json({ error: 'Invalid token' });
+  }
+});
+
+
+
+
 
 
 app.listen(PORT, () => {
--- services/database.js
+++ services/database.js
@@ -15,14 +15,15 @@ async function getTweets() {
       tweets.message,
       tweets.created_at,
       users.name,
-      users.username
+      users.username,
+      tweets.like_count
     FROM
       tweets
     INNER JOIN users ON
       tweets.user_id = users.id
     ORDER BY created_at DESC;
   `);
-  //console.log(result);
+  console.log(result.rows);
   return result.rows;
 }
 
@@ -33,7 +34,8 @@ async function getTweetsByUsername(username) {
       tweets.message,
       tweets.created_at,
       users.name,
-      users.username
+      users.username,
+      tweets.like_count
     FROM
       tweets
     INNER JOIN users ON
@@ -42,7 +44,7 @@ async function getTweetsByUsername(username) {
       users.username = $1
     ORDER BY created_at DESC;
   `, [username]);
-
+  console.log(result.rows)
   return result.rows;
 }
 
@@ -70,8 +72,64 @@ async function createTweet(username, text) {
   return newTweet;
 }
 
+async function getUserByUsername(username) {
+  const result = await database.query(`
+    SELECT 
+      *
+    FROM
+      users
+    WHERE
+      username = $1
+  `, [username]);
+  const user = result.rows[0];
+  return user;
+}
+
+async function updateLikes(tweetId, like_count) { 
+  const likeResult = await database.query(`
+    UPDATE tweets
+    SET like_count = $2
+    WHERE id = $1
+    RETURNING
+      like_count
+  `, [tweetId, (like_count + 1)]);
+  const newLike = likeResult.rows[0];
+  return newLike;
+}
+
+async function defineLiked(tweetId, user_id) {
+  const likeResult = await database.query(`
+    Update likes
+    SET liked = true
+    WHERE tweet_id = $1 AND user_id = $2
+    RETURNING
+      liked
+  `, [tweetId, user_id]);
+  const like = likeResult.rows[0];
+  return like;
+}
+
+async function handleCheckLikes(tweetId, user_id) {
+  const likeResult = await database.query(`
+    SELECT
+      *
+    FROM
+      likes
+    WHERE
+      tweet_id = $1 AND user_id = $2
+  `, [tweetId, user_id]);
+  const like = likeResult.rows[0];
+  return like;
+}
+
+
+
 module.exports = {
   getTweets,
   getTweetsByUsername,
   createTweet,
+  getUserByUsername,
+  updateLikes,
+  handleCheckLikes,
+  defineLiked
 };
